9ce67fe37ec2230ccce78d63e62c8173fdb1dd0c
Conan <conantorreswf@gmail.com>
2026-02-26 20:29:42 -0300

    chore: ajustes de permissões nos arquivos

--- dockerfiles/php/entrypoint.sh
+++ dockerfiles/php/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 set -e
+# Ownership for PHP-FPM; then permissive so host user can edit/delete (e.g. laravel.log)
 chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
+chmod -R 777 /var/www/storage /var/www/bootstrap/cache
 exec "$@"
